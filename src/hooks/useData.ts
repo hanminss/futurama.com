cc14816323af8796641448a6ba9e8443a66fcddc
HanMinSS <gksals6464@naver.com>
2021-12-23 22:22:04 +0900

    ➕  Setting axios, endpoint

---
+++ src/hooks/useData.ts
@@ -0,0 +1,7 @@
+import { fetcher } from "./../utils/fetcher";
+import { API_END_POINT } from "./../constants/index";
+import useSWR from "swr";
+
+export const useData = (path: string) => {
+  return useSWR(`${API_END_POINT}`, fetcher);
+};
