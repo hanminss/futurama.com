06f06186f76d10e55d2f90f7269bf0598ede558b
HanMinSS <gksals6464@naver.com>
2021-12-27 02:52:41 +0900

    ✅  인벤토리 마크업 로직 구현

--- src/pages/inventory.tsx
+++ src/pages/inventory.tsx
@@ -1,8 +1,48 @@
 import type { NextPage } from "next";
+import React, { useState } from "react";
 import { Error } from "../components/Error";
+import { Loading } from "../components/Loading";
+import { useData } from "../hooks/useData";
+
+interface invType {
+  title: string;
+  category: string;
+  description: string;
+  slogan: string;
+  price: string;
+  stock: string;
+  id: number;
+}
 
 const Inventory: NextPage = () => {
-  return <Error />;
+  const randomSeed = (() => {
+    return Math.floor(Math.random() * 31) + 1;
+  })();
+
+  const [num, setNum] = useState(0);
+
+  const handleChangeId = () => {
+    setNum(randomSeed);
+  };
+  const { data, error } = useData("inventory");
+  if (error) return <Error />;
+  if (!data) return <Loading />;
+
+  if (num == 0) return <button onClick={handleChangeId}>find</button>;
+  const randomBox = data.find((datas: invType) => datas.id == num);
+  const { title, category, description, slogan, price, stock, id } = randomBox;
+
+  return (
+    <main>
+      <button onClick={handleChangeId}>find</button>
+      <p>{title}</p>
+      <p>{category}</p>
+      <p>{description}</p>
+      <p>{slogan}</p>
+      <p>{price}</p>
+      <p>{stock}</p>
+    </main>
+  );
 };
 
 export default Inventory;
